9c9e968240617a186c7a9e13fec91236ff3e5953
carolventura1996@gmail.com <carolventura1996@gmail.com>
2021-10-24 22:54:07 +0100

    final project

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: "venturcarol",
+  youtube: "kG97ua8QW5_8-SyfrpAiYg",
+  facebook: "maykbrito",
+  instagram: "cventurr",
+  twitter: "venturcarol"
+}
+
+function changeSocialMediaLinks(){
+  for(let li of socialLinks.children){
+    const social = li.getAttribute('class');
+    
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userPhoto.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+getGitHubProfileInfos()
